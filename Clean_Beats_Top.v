980c1273a7db4ecaba8308ffe075fc2b120f1662
HealixZero <healixzero@gmail.com>
2013-10-01 20:46:36 -0700

    Re-did entire build
    Rebuilt from the ground up to get rid of an TLB vector error.
    new Qsys files and quartus files

---
+++ Clean_Beats_Top.v
@@ -0,0 +1,9 @@
+/*******************************************************************************
+Brent Honzaki
+Senior Project: Clean Beats
+Bluetooth-based waterproof stereo system
+
+*******************************************************************************/
+
+module Clean_Beats (
+	(* chip_pin = ""))
